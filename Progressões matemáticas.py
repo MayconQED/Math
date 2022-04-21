a8000310b4aff4680ac6c4c8d3a8d8ec96a56ba5
mike <91846209+laptop2k@users.noreply.github.com>
2022-04-21 12:33:39 -0300

    Update Progressões matemáticas.py

--- Progressões matemáticas.py
+++ Progressões matemáticas.py
@@ -1,53 +1,75 @@
+from time import sleep
+
 def PA():
     #Valor de "an" em uma P.A
-    a1=int(input('Digite o valor do primeiro termo:'))
-    n=int(input('Digite a posição do termo a procura:'))
-    r=int(input('Digite a razão da P.A:'))
-    an = a1+(n-1)*r
-    print('O valor de "An" é igual a:', an)
+    k = int(input('Digite a posição do termo conhecido: '))
+    ak = int(input('Digite o valor do termo conhecido: '))
+    n = int(input('Digite a posição do termo a procura: '))
+    r = int(input('Digite a razão da P.A: '))
+    an = ak+(n-k)*r
+    print('O valor de "An" é igual a: {}'.format(an))
+    print('==='*20)
+    sleep(1)
 
 def PG():
     #Valor de "an" em uma P.G
-    a1=int(input('Digite o valor do primeiro termo:'))
-    n=int(input('Digite a posição do termo a procura:'))
-    q=int(input('Digite a razão da P.G:'))
-    an=a1*q**(n-1)
-    print('O valor de "An" é igual a:', an)
-
+    k = int(input('Digite a posição do termo conhecido: '))
+    ak = int(input('Digite o valor do termo conhecido: '))
+    n = int(input('Digite a posição do termo a procura: '))
+    q = int(input('Digite a razão da P.G: '))
+    an = ak*q**(n-k)
+    print('O valor de "an" é igual a: {}'.format(an))
+    print('==='*20)
+    sleep(1)
+    
 def SPA():
     #Valor de "Sn" em uma soma de uma P.A
-    a1=int(input('Digite o valor do primeiro termo:'))
-    an=int(input('Digite o valor de "An" encontrado com auxílio do código acima:'))
-    n=int(input('Digite a posição do termo de "An":'))
-    Sn=(a1+an)*n/2
-    print('O valor da soma dessa P.A é igual a:', Sn)
+    a1 = int(input('Digite o valor do primeiro termo: '))
+    an = int(input('Digite o valor de "an": '))
+    n = int(input('Digite a posição do termo de "an": '))
+    Sn = (a1+an)*n/2
+    print('O valor da soma dessa P.A é igual a: {}'.format(Sn))
+    print('==='*20)
+    sleep(1)
 
 def SPG():
     #Valor de "Sn" em uma soma de uma P.G
-    a1=int(input('Digite o valor do primeiro termo:'))
-    an=int(input('Digite o valor de "An" encontrado com auxílio do código acima:'))
-    n=int(input('Digite a posição do termo de "An":'))
-    q=int(input('Digite a razão dessa P.G:'))
-    Sn=a1*((q**n)-1)/(q-1)
-    print('O valor da soma dessa P.A é igual a:', Sn)
+    a1 = int(input('Digite o valor do primeiro termo: '))
+    n = int(input('Digite a posição do termo de "an": '))
+    q = int(input('Digite a razão dessa P.G: '))
+    Sn = a1*((q**n)-1)/(q-1)
+    print('O valor da soma dessa P.G é igual a: {}'.format(Sn))
+    print('==='*20)
+    sleep(1)
 
-opcao=1
+op=1
+print('==='*20)
+print('Escolha uma das opções para calcular:')
+print('==='*20)
 
-while opcao:
+while op:
     print("0. Sair")
-    print("1. PA")
-    print("2. PG")
-    print("3. SPA")
-    print("4. SPG")
-
-    opcao = int(input("Opção: "))
-
-    if(opcao==1):
+    print("1. P. Aritmética")
+    print("2. P. Geométrica")
+    print("3. Soma de uma   P. Aritmética")
+    print("4. Soma de uma P. Geométrica")
+ 
+    op = int(input("Opção: "))
+    print('==='*20)
+    if (op==1):
         PA()
-    if(opcao==2):
+    elif (op==2):
         PG()
-    if(opcao==3):
+    elif op==3:
         SPA()
-    if(opcao==4):
-        SPG()
-
+    elif (op==4):
+        SPG()   
+    elif (op==0):
+        print('Saindo...')
+        sleep(3)
+        print('Programa fechou!')
+        print('==='*20)
+    else:
+        print('Opção invalida, tente novamente.')
+        print('==='*20)
+        sleep(2)
